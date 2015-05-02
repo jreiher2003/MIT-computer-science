2ba34fd5548592f2b46efc100c763f96882f9440
Jeff Reiher <jeffreiher@gmail.com>
2015-05-01 20:12:39 -0400

    first lesson

---
+++ 1 Data types, operators and variables/assign1.py
@@ -0,0 +1,10 @@
+# Problem set 0
+# Name: Jeff Reiher
+# Time: 5 minutes
+
+# ask use what first and last name are
+import this
+print 'Whats your first name',
+first_name = raw_input()
+print 'What is your last name',
+last_name = raw_input()
