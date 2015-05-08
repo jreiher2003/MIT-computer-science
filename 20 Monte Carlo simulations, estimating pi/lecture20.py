d60e80850f6c12e71ff86aff64b19de74412e807
Jeff Reiher <jeffreiher@gmail.com>
2015-05-08 09:36:27 -0400

    coin flips

---
+++ 20 Monte Carlo simulations, estimating pi/lecture20.py
@@ -0,0 +1,42 @@
+from pylab import *
+import random, math
+
+def flipTrial(numFlips):
+	heads, tails = 0, 0
+	for i in xrange(0, numFlips):
+		coin = random.randint(0,1)
+		if coin == 0:
+			heads += 1
+		else:
+			tails += 1
+	return heads, tails
+
+
+def simFlips(numFlips, numTrials):
+	diffs = []
+	for i in xrange(0, numTrials):
+		heads, tails = flipTrial(numFlips)
+		diffs.append(abs(heads-tails))
+	diffs = array(diffs)
+	diffMean = sum(diffs)/len(diffs)
+	diffPercent = (diffs/float(numFlips))*100
+	percentMean = sum(diffPercent)/len(diffPercent)
+	axvline(diffMean, label = 'Mean')
+	legend()
+	titleString = str(numFlips) + 'Flips, ' + str(numTrials) + 'Trials '
+	title(titleString)
+	xlabel('Difference between heads and tails')
+	ylabel('Number of Trials')
+	figure()
+	plot(diffPercent)
+	axhline(percentMean, label = 'Mean')
+	legend()
+	title(titleString)
+	xlabel('Trial Number: ')
+	ylabel('Percent Difference between heads and tails')
+
+# simFlips(1000,100)
+# figure()
+# simFlips(5000, 100)
+simFlips(3, 4000)
+show()
